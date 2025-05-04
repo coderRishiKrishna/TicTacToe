42f1f33884b74b7de08b4ee63337c0d775b4a265
coderRishiKrishna <yrishikrishna@gmail.com>
2025-05-04 18:52:33 +0530

    Added a draw condition..

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
+def game_board(rState, kState):
+    board = []
+    for i in range (9):
+        if rState[i]:
+            board.append('R')
+        elif kState[i]:
+            board.append('K')
+        else:
+            board.append(str(i))
+
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print(f"--|---|---")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print(f"--|---|---")
+    print(f"{board[6]} | {board[7]} | {board[8]}") 
+
 def check_sum(a, b, c):
     return a+ b+ c
-
-def game_board(rState, kState):
-    zero = 'R' if rState[0] else ('K' if kState[0] else 0)
-    one = 'R' if rState[1] else ('K' if kState[1] else 1)
-    two = 'R' if rState[2] else ('K' if kState[2] else 2)
-    three = 'R' if rState[3] else ('K' if kState[3] else 3)
-    four = 'R' if rState[4] else ('K' if kState[4] else 4)
-    five = 'R' if rState[5] else ('K' if kState[5] else 5)
-    six = 'R' if rState[6] else ('K' if kState[6] else 6)
-    seven = 'R' if rState[7] else ('K' if kState[7] else 7)
-    eight = 'R' if rState[8] else ('K' if kState[8] else 8)
-    print(f"{zero} | {one} | {two} ")
-    print(f"--|---|---")
-    print(f"{three} | {four} | {five} ")
-    print(f"--|---|---")
-    print(f"{six} | {seven} | {eight} ") 
-
 
 def check_win(rState, kState):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -48,6 +47,11 @@ if __name__ == "__main__":
             kState[value] = 1
         cwin = check_win(rState, kState)
         if(cwin != -1):
+            print("Match over")
+            break
+
+        if sum(rState) + sum(kState) == 9:
+            print("It's a draw!")
             print("Match over")
             break
         
